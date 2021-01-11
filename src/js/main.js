3cd8d125156fe19d4aceaf9212d15644bfc89ca8
dikaMuharman <dika1254@gmail.com>
2021-01-11 09:04:44 +0700

    menghilangkan form dan mengganti btn cta, menambah halaman

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
 import "../css/main.scss";
 import "bootstrap";
+import Tagify from "@yaireo/tagify";
+
+// let inputCategory = document.getElementById("inputCategory");
+let category = ["senior programmer", "Junior programmer", "ui/ux research"];
+
+new Tagify(inputCategory);
