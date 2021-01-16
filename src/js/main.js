59dd7d5643f2c84fd108a22e018d5c15645fc557
dikaMuharman <dika1254@gmail.com>
2021-01-16 10:32:00 +0700

    revisi tampilan + add login modal

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,46 @@ import "../css/main.scss";
 import "bootstrap";
 import Tagify from "@yaireo/tagify";
 
-// let inputCategory = document.getElementById("inputCategory");
-let category = ["senior programmer", "Junior programmer", "ui/ux research"];
+document.addEventListener("DOMContentLoaded", () => {
+  const email = document.getElementById("EmailLogin");
+  const password = document.getElementById("PasswordLogin");
+  const formLogin = document.getElementById("FormLogin");
+  const emailRegex = /[^@ \t\r\n]+@[^@ \t\r\n]+\.[^@ \t\r\n]+/;
+  const passwordRegex = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/;
 
-new Tagify(inputCategory);
+  formLogin?.addEventListener("submit", (e) => checkLogin(e));
+
+  const checkLogin = (e) => {
+    if (emailRegex.test(email.value)) {
+      console.log("email is valid");
+      email.classList.remove("is-invalid");
+      email.classList.add("is-valid");
+
+      if (email.value !== "") {
+        email.classList.remove("is-invalid");
+        email.classList.add("is-valid");
+      } else {
+        email.classList.add("is-invalid");
+        e.preventDefault();
+      }
+    } else {
+      email.classList.remove("is-valid");
+      email.classList.add("is-invalid");
+      e.preventDefault();
+    }
+
+    if (password.value !== "") {
+      password.classList.remove("is-invalid");
+      password.classList.add("is-valid");
+    } else {
+      password.classList.add("is-invalid");
+      e.preventDefault();
+    }
+
+    // if (passwordRegex.test(password.value)) {
+    //   password.classList.add("is-valid");
+    // } else {
+    //   error.push("password");
+    // }
+  };
+});
