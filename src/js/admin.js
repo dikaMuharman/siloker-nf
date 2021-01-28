80376c21a62f82dcb608f5befb9931169c28f9e0
dikaMuharman <dika1254@gmail.com>
2021-01-28 23:55:53 +0700

    menambahkan icon dan dialog delete

--- src/js/admin.js
+++ src/js/admin.js
@@ -1,1 +1,12 @@
+const modalComponent = new bootstrap.Modal(
+  document.querySelector(".example-modal")
+);
 
+const linksDelete = document.querySelectorAll(".link-danger");
+
+linksDelete.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+    modalComponent.show();
+  });
+});
